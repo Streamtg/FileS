e017c9902c62f413238fd6337376cd7544171e04
Streamtg <202310475@issu.edu.do>
2025-07-26 16:24:16 -0400

    Update main.go

--- cmd/fsb/main.go
+++ cmd/fsb/main.go
@@ -1,13 +1,66 @@
+Skip to content
+Navigation Menu
+EverythingSuckz
+TG-FileStreamBot
+
+Type / to search
+Code
+Issues
+29
+Pull requests
+3
+Discussions
+Actions
+Projects
+1
+Security
+Insights
+Files
+Go to file
+t
+.github
+.vscode
+cmd/fsb
+main.go
+run.go
+session.go
+config
+internal
+pkg
+.gitattributes
+.gitignore
+.goreleaser.yaml
+Dockerfile
+LICENSE
+Procfile
+README.md
+app.json
+docker-compose.yaml
+fsb.sample.env
+go.mod
+go.sum
+goreleaser.Dockerfile
+TG-FileStreamBot/cmd/fsb
+/main.go
+EverythingSuckz
+EverythingSuckz
+chore(version): release 3.1.0
+6261681
+ · 
+5 months ago
+
+Code
+
+Blame
+37 lines (31 loc) · 851 Bytes
 package main
 
 import (
 	"EverythingSuckz/fsb/config"
 	"fmt"
 	"os"
-	"github.com/spf13/cobra""fmt"
-	"html/template"
-	"net/http"
-	
+
+	"github.com/spf13/cobra"
 )
 
 const versionString = "3.1.0"
