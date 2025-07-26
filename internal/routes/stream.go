35d66693dcff4e060a5b5d08b58d21f51a9f0883
Streamtg <202310475@issu.edu.do>
2025-07-26 19:42:50 -0400

    Update stream.go

--- internal/routes/stream.go
+++ internal/routes/stream.go
@@ -4,24 +4,28 @@ import (
 	"fmt"
 	"net/http"
 
-	"EverythingSuckz/fsb/config"
+	"EverythingSuckz/fsb/internal/config"
 	"github.com/gin-gonic/gin"
 )
 
+// Stream renderiza el reproductor HTML con el enlace del archivo directo
 func Stream(c *gin.Context) {
 	fileID := c.Param("fileID")
 	fileName := c.Param("fileName")
 
 	if fileID == "" || fileName == "" {
-		c.String(http.StatusBadRequest, "Faltan parámetros en la URL.")
+		c.String(http.StatusBadRequest, "Parámetros inválidos.")
 		return
 	}
 
-	streamURL := fmt.Sprintf("%s/dl/%s/%s", config.GetBaseURL(), fileID, fileName)
+	// Genera la URL del archivo que será usada por el reproductor
+	// Esta URL debe ser manejada por un handler que sirva el archivo (como /dl/:fileID/:fileName)
+	baseURL := config.Get().BaseURL
+	streamURL := fmt.Sprintf("%s/dl/%s/%s", baseURL, fileID, fileName)
 
+	// Renderiza el template del reproductor
 	c.HTML(http.StatusOK, "stream.html", gin.H{
-		"fileID":    fileID,
-		"fileName":  fileName,
-		"streamURL": streamURL,
+		"StreamURL": streamURL,
+		"FileName":  fileName,
 	})
 }
