3dc13c03089d6055903dd83448bcbcdcf9bcc71b
Streamtg <202310475@issu.edu.do>
2025-07-26 18:13:06 -0400

    Update main.go

--- cmd/fsb/main.go
+++ cmd/fsb/main.go
@@ -13,30 +13,25 @@ const versionString = "3.1.0"
 var rootCmd = &cobra.Command{
 	Use:               "fsb [command]",
 	Short:             "Telegram File Stream Bot",
-	Long:              "Telegram Bot to generate direct streamable links for Telegram media.",
+	Long:              "Telegram Bot to generate direct streamable links for telegram media.",
 	Example:           "fsb run --port 8080",
 	Version:           versionString,
 	CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
 	Run: func(cmd *cobra.Command, args []string) {
-		_ = cmd.Help()
+		cmd.Help()
 	},
 }
 
 func init() {
-	// Establece flags de configuración desde el archivo de configuración
 	config.SetFlagsFromConfig(runCmd)
-
-	// Registra los subcomandos
 	rootCmd.AddCommand(runCmd)
 	rootCmd.AddCommand(sessionCmd)
-
-	// Establece el template de versión
 	rootCmd.SetVersionTemplate(fmt.Sprintf(`Telegram File Stream Bot version %s`, versionString))
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
